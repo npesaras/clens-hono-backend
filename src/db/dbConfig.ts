e32bdf01dbb9af3217056853fff5c6e6d22c72cb
npesaras <nilmar.pesarast@gmail.com>
2025-04-21 15:50:39 +0800

    Fix: postgres-js to node-postgres drizzle driver

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -1,4 +1,4 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import pkg from 'pg';
 const { Pool } = pkg;
