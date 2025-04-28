87ced2ede81781ed32f7cf0a5119fe2aee7d0816
npesaras <nilmar.pesarast@gmail.com>
2025-04-28 20:23:02 +0800

    Feature: added swagger-ui

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { swaggerUI } from '@hono/swagger-ui'
 import { errorHandlerMiddleware } from '@/middlewares/error-handler'
 import { routes } from '@/controllers/routes'
 import { env } from '@/utils/env'
@@ -7,13 +8,19 @@ import { db } from '@/db/dbConfig'
 import { cors } from 'hono/cors'
 import { sql } from 'drizzle-orm'
 import { users } from '@/db/schema'
+import { openApiDocument } from '@/utils/openapi/definition'
+import { Context } from 'hono'
 
-
+// Create Hono app
 const app = new Hono()
 
 // Middleware
 app.use('*', cors())
 app.onError(errorHandlerMiddleware)
+
+// Swagger UI
+app.get('/api-docs/json', (c: Context) => c.json(openApiDocument))
+app.use('/api-docs/*', swaggerUI({ url: '/api-docs/json' }))
 
 /* Health Check */
 app.get(env.HEALTH_CHECK_PATH, async (c) => {
@@ -51,4 +58,5 @@ serve({
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
   console.log(`Health check available at: http://localhost:${info.port}${env.HEALTH_CHECK_PATH}`)
+  console.log(`API documentation available at: http://localhost:${info.port}/api-docs`)
 })
